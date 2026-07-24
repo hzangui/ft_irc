fb904f4e9ec83a34823b86bc41e085656fd69cb3
Hamza Zangui <hzangui@c1r6p14.1337.ma>
2026-07-24 20:07:31 +0100

    finish some command main checks

--- src/cmd/KickCmd.cpp
+++ src/cmd/KickCmd.cpp
@@ -11,5 +11,27 @@ KickCmd::KickCmd(Server &server) : ACmd(server, true)
 
 void KickCmd::execute(cmdCtx &ctx)
 {
+	Channel *channel = server.findChannel(ctx.params[0]);
+	Client *target;
 
+	if(!channel)
+	{
+		//channel not existe;
+	}
+	if(!channel->isMember(ctx.client))
+	{
+		// sender not member of channel;
+	}
+	if(!channel->isOperator(ctx.client))
+	{
+		// sender not a operator;
+	}
+	target = channel->findMember(ctx.params[1]);
+	if (!target)
+	{
+		// target not in the channel;
+	}
+	//send msg brodcast;
+	channel->removeMember(target);
+	target->leaveChannel(channel);
 }
--- src/cmd/TopicCmd.cpp
+++ src/cmd/TopicCmd.cpp
@@ -1,9 +1,34 @@
-#include "../../include/Cmd.hpp"
+#include "../../include/cmd/TopicCmd.hpp"
+#include "../../include/Server.hpp"
+#include "../../include/Channel.hpp"
+#include "../../include/Client.hpp"
 
 TopicCmd::TopicCmd(Server &server):ACmd(server, true)
 {}
 
 void TopicCmd::execute(cmdCtx &ctx)
 {
-    ch
+    Channel *channel = server.findChannel(ctx.params[0]);
+    
+    if (!channel)
+    {
+        //channel not exist;
+    }
+    if(ctx.lastArg.empty())
+    {
+        if (channel->getTopic().empty())
+        {
+            //channel have empty topic;
+        }
+        else
+        {
+            //msg with topic of channel;
+        }
+    }
+    if (channel->topicRestricted() && !channel->isOperator(ctx.client))
+    {
+        // client not a operator;
+    }
+    //msg brodcast
+    channel->setTopic(ctx.lastArg);
 }
